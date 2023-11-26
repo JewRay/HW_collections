053da0762c3cc6a08f5287faf8701c3bbae8b95f
JewRay <sigizmod@gmail.com>
2023-11-26 19:06:01 +0300

    Initial commit

---
+++ src/main/java/com/example/javaKurs/JavaKursApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaKurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKursApplication.class, args);
+	}
+
+}
